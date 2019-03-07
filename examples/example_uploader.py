a42c4315059c159d0ffa55373e2b0113fc87d464
mowoe <github@mowoe.com>
2019-03-07 21:06:56 +0100

    added sample uploader, that uploads all files in a given folder

---
+++ examples/example_uploader.py
@@ -0,0 +1,22 @@
+#This File uploads all images from a directory to heartbeat
+
+import requests
+import sys
+from os import listdir
+from os.path import isfile, join
+
+host,port = "localhost",str(9721)
+
+mypath = sys.argv[1]
+
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+for fil in onlyfiles:
+    if "jpg" in fil or "png" in fil or "jpeg" in fil:
+        print("uploading: "+mypath+fil)
+        files = {'file': open(mypath+fil,'rb')}
+        #start = time.time()
+        r = requests.post("http://"+host+":"+port+"/add_image_via_file", files=files, allow_redirects=False)
+        print(r.text)
+
+print("done")
