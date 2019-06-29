cf4b79c6933c62b72338a7574825385660fc230f
mowoe <github@mowoe.com>
2019-06-29 07:30:56 +0200

    added exit code in testing script

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
+curl "http://localhost:5000/"
 if [ $(curl -s -o /dev/null -w "%{http_code}" http://localhost:5000/ | cut -c1-3) == "200" ]; then
-	curl "http://localhost:5000/"
 	echo "success"
+	exit 0
 else
 	echo "error" >&2
+	exit 1
 fi
