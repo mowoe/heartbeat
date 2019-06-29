9cf7e4fe63a946e200d92efe8158d47af3db202e
mowoe <github@mowoe.com>
2019-06-29 13:51:37 +0200

    docker push to registry added

--- test.sh
+++ test.sh
@@ -1,8 +1,13 @@
-curl "http://localhost:5000/"
+echo "logging docker in"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "pushing to docker..."
+docker tag carlad/sinatra mowoe/heartbeat:latest
+docker push mowoe/heartbeat:latest
 if [ $(curl -s -o /dev/null -w "%{http_code}" http://localhost:5000/ | cut -c1-3) == "200" ]; then
 	echo "success"
 	exit 0
 else
-	echo "error" >&2
+	curl "http://localhost:5000/"
+	echo "error"
 	exit 1
 fi
