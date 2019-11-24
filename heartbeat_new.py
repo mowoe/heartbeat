d50baea5b26d0540561a60c490e445ca6760caeb
mowoe <github@mowoe.com>
2019-11-24 13:07:10 +0100

    code refactoring

--- heartbeat_new.py
+++ heartbeat_new.py
@@ -16,7 +16,7 @@ import requests
 import argparse
 import urllib
 from heartbeat_db import HeartbeatDB
-import sys  
+import sys
 import peewee
 import read_config
 
@@ -30,58 +30,62 @@ mysql_db = heartbeat_db.init_db(
     heartbeat_config.config["db_type"],
     heartbeat_config.config["db_auth"],
     heartbeat_config.config["object_storage_type"],
-    heartbeat_config.config["object_storage_auth"]
+    heartbeat_config.config["object_storage_auth"],
 )
 
 
-UPLOAD_FOLDER = './uploaded_pics/'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+UPLOAD_FOLDER = "./uploaded_pics/"
+ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg", "gif"])
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 model_path = "./examples/trained_knn_model.clf"
 distance_threshold = 0.5
 near_images_to_show = 5
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def constr_resp(status,reason="healthy"):
-        return json.dumps({'status':status, 'reason':reason})
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def constr_resp(status, reason="healthy"):
+    return json.dumps({"status": status, "reason": reason})
+
 
 @app.before_request
 def _db_connect():
-    if heartbeat_config.config["db_type"]=="mysql":
+    if heartbeat_config.config["db_type"] == "mysql":
         mysql_db.connect()
+
 
 @app.teardown_request
 def _db_close(exc):
-    if heartbeat_config.config["db_type"]=="mysql":
+    if heartbeat_config.config["db_type"] == "mysql":
         if not mysql_db.is_closed():
-                mysql_db.close()
+            mysql_db.close()
 
-@app.route("/api/add_image",methods=['POST'])
+
+@app.route("/api/add_image", methods=["POST"])
 def add_image():
     try:
-        img_url = request.form.get('img_url')
-        information = request.form.get('img_info')
-        origin = request.form.get('origin')
+        img_url = request.form.get("img_url")
+        information = request.form.get("img_info")
+        origin = request.form.get("origin")
         if type(img_url) == type(None) or type(information) == type(None):
-            return constr_resp("error","No url or Image provided")
+            return constr_resp("error", "No url or Image provided")
         if ".png" in img_url:
             fend = ".png"
         elif ".jpg" in img_url:
             fend = ".jpg"
         else:
-            return constr_resp("error","no correct fileformat")
+            return constr_resp("error", "no correct fileformat")
         time_hash = hashlib.sha256(str(time.time()).encode()).hexdigest()
         new_filename = str(time_hash) + fend
-        urllib.request.urlretrieve(img_url,os.path.join(UPLOAD_FOLDER, new_filename))
+        urllib.request.urlretrieve(img_url, os.path.join(UPLOAD_FOLDER, new_filename))
         information = json.loads(information)
         information = json.dumps(information)
-        heartbeat_db.upload_file(new_filename,origin,information)
+        heartbeat_db.upload_file(new_filename, origin, information)
         return constr_resp("success")
     except peewee.InterfaceError as e:
         print("PeeWee Interface broken!")
@@ -89,81 +93,126 @@ def add_image():
             heartbeat_config.config["db_type"],
             heartbeat_config.config["db_auth"],
             heartbeat_config.config["object_storage_type"],
-            heartbeat_config.config["object_storage_auth"]
+            heartbeat_config.config["object_storage_auth"],
         )
         print(e)
-        return constr_resp("database error","if this error keeps occuring contact admin")
+        return constr_resp(
+            "database error", "if this error keeps occuring contact admin"
+        )
     except Exception as e:
         print(e)
-        return constr_resp("error","unknown error, maybe not all query parameters were specified?")
+        return constr_resp(
+            "error", "unknown error, maybe not all query parameters were specified?"
+        )
 
-@app.route("/api/request_work",methods=['GET'])
+
+@app.route("/api/request_work", methods=["GET"])
 def request_work():
-    work_type = request.args.get('work_type')
-    results=heartbeat_db.request_work(work_type)
+    work_type = request.args.get("work_type")
+    results = heartbeat_db.request_work(work_type)
     if len(results) > 0:
         return constr_resp(str(results[0]))
     else:
-        return constr_resp("error","empty")
+        return constr_resp("error", "empty")
 
-@app.route("/api/submit_work",methods=['POST'])
+
+@app.route("/api/submit_work", methods=["POST"])
 def submit_work():
     start = time.time()
-    work_type = request.form.get('work_type')
-    img_id = request.form.get('image_id')
-    resulted = request.form.get('result')
-    heartbeat_db.submit_work(work_type,img_id,resulted)
-    print("submit work took {} seconds".format(time.time()-start))
+    work_type = request.form.get("work_type")
+    img_id = request.form.get("image_id")
+    resulted = request.form.get("result")
+    heartbeat_db.submit_work(work_type, img_id, resulted)
+    print("submit work took {} seconds".format(time.time() - start))
     return constr_resp("success")
+
 
 @app.route("/api/get_all_work")
 def get_all_work():
-    work_type = request.args.get('work_type')
+    work_type = request.args.get("work_type")
     results = heartbeat_db.get_all_work(work_type)
-    return json.dumps({"result":json.dumps(results)})
+    return json.dumps({"result": json.dumps(results)})
+
 
 @app.route("/api/download_image")
 def download_image():
-    imgid = request.args.get('image_id')
+    imgid = request.args.get("image_id")
     resp = heartbeat_db.get_file(imgid)
     if type(resp) == type(None):
-        resp = render_template("error.html",errormessage="Das Bild scheint nicht mehr vorhanden zu sein. Sorry!")
+        resp = render_template(
+            "error.html",
+            errormessage="Das Bild scheint nicht mehr vorhanden zu sein. Sorry!",
+        )
     return resp
 
-@app.route("/api/get_matching_images",methods=['POST'])
+
+@app.route("/api/get_matching_images", methods=["POST"])
 def get_matching_images():
-    with open(model_path, 'rb') as f:
-            knn_clf = pickle.load(f)
-    file = request.files['file']
+    with open(model_path, "rb") as f:
+        knn_clf = pickle.load(f)
+    file = request.files["file"]
     file.save("facerec_img.png")
     X_img = face_recognition.load_image_file("facerec_img.png")
     X_face_locations = face_recognition.face_locations(X_img)
     if len(X_face_locations) == 0:
         return []
-    faces_encodings = face_recognition.face_encodings(X_img, known_face_locations=X_face_locations)
+    faces_encodings = face_recognition.face_encodings(
+        X_img, known_face_locations=X_face_locations
+    )
 
     closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
-    are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
-    print(json.dumps([(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]))
-    print([(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)])
-    return json.dumps({"result":[(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]})
+    are_matches = [
+        closest_distances[0][i][0] <= distance_threshold
+        for i in range(len(X_face_locations))
+    ]
+    print(
+        json.dumps(
+            [
+                (pred, loc) if rec else ("unknown", loc)
+                for pred, loc, rec in zip(
+                    knn_clf.predict(faces_encodings), X_face_locations, are_matches
+                )
+            ]
+        )
+    )
+    print(
+        [
+            (pred, loc) if rec else ("unknown", loc)
+            for pred, loc, rec in zip(
+                knn_clf.predict(faces_encodings), X_face_locations, are_matches
+            )
+        ]
+    )
+    return json.dumps(
+        {
+            "result": [
+                (pred, loc) if rec else ("unknown", loc)
+                for pred, loc, rec in zip(
+                    knn_clf.predict(faces_encodings), X_face_locations, are_matches
+                )
+            ]
+        }
+    )
+
 
 @app.route("/")
 def main():
     return render_template("index.html")
 
+
 @app.route("/upload")
 def upload_frontend():
     return render_template("upload.html")
 
-@app.route("/get_matching_images",methods=['POST'])
+
+@app.route("/get_matching_images", methods=["POST"])
 def frontend_matching_images():
     if heartbeat_config.config["db_type"] == "s3":
         print("downloading image model from bucket...")
         heartbeat_db.retrieve_model(model_path)
-    with open(model_path, 'rb') as f:
+    with open(model_path, "rb") as f:
         knn_clf = pickle.load(f)
-    file = request.files['file']
+    file = request.files["file"]
     file.save(file.filename)
     print(file.filename)
     try:
@@ -171,12 +220,22 @@ def frontend_matching_images():
         X_face_locations = face_recognition.face_locations(X_img)
     except TypeError:
         print("TypeError Catched!")
-        return render_template("error.html",errormessage="Ein Fehler ist aufgetreten, ist eventuell kein Gesicht auf dem Bild oder ist das Bild zu gross?")
+        return render_template(
+            "error.html",
+            errormessage="Ein Fehler ist aufgetreten, ist eventuell kein Gesicht auf dem Bild oder ist das Bild zu gross?",
+        )
     if len(X_face_locations) == 0:
-        return render_template("error.html",errormessage="Wir konnten kein Gesicht auf deinem Bild finden!")
-    faces_encodings = face_recognition.face_encodings(X_img, known_face_locations=X_face_locations)
-    closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=near_images_to_show)
-    with open("./trained_knn_list.clf",'rb') as f:
+        return render_template(
+            "error.html",
+            errormessage="Wir konnten kein Gesicht auf deinem Bild finden!",
+        )
+    faces_encodings = face_recognition.face_encodings(
+        X_img, known_face_locations=X_face_locations
+    )
+    closest_distances = knn_clf.kneighbors(
+        faces_encodings, n_neighbors=near_images_to_show
+    )
+    with open("./trained_knn_list.clf", "rb") as f:
         all_labels = pickle.load(f)
     print(closest_distances[1][0])
     res = []
@@ -187,28 +246,29 @@ def frontend_matching_images():
             labels = []
             image = heartbeat_db.get_imgobj_from_id(label)
             origin = image.origin
-            labels.append("origin: "+origin)
+            labels.append("origin: " + origin)
             other_data = json.loads(image.other_data)
             if type(other_data) != type(None):
                 for key in other_data:
                     if len(str(other_data[key])) > 0:
-                        labels.append(key+": "+str(other_data[key]))    
-            res.append({"id":label,"score":str(score)[:5],"labels":labels})
+                        labels.append(key + ": " + str(other_data[key]))
+            res.append({"id": label, "score": str(score)[:5], "labels": labels})
 
     print(res)
     os.remove(file.filename)
-    return render_template("result.html",images=res)
+    return render_template("result.html", images=res)
 
-@app.route("/admin",methods=['GET'])
+
+@app.route("/admin", methods=["GET"])
 def admin_panel():
-    action = request.args.get('action')
+    action = request.args.get("action")
     if type(action) != type(None):
         try:
             if action == "update_knn":
                 X = []
                 y = []
                 counter = 0
-                work_type = 'face_encodings'
+                work_type = "face_encodings"
                 results = heartbeat_db.get_all_work(work_type)
                 all_encodings = results
                 for encoding in all_encodings:
@@ -216,25 +276,27 @@ def admin_panel():
                     if len(face_bounding_boxes) > 2:
                         X.append(np.array(face_bounding_boxes))
                         y.append(encoding[0])
-                        counter+=1
+                        counter += 1
                 print("found {} encodings".format(counter))
 
                 n_neighbors = int(round(math.sqrt(len(X))))
 
-                knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm="ball_tree", weights='distance')
+                knn_clf = neighbors.KNeighborsClassifier(
+                    n_neighbors=n_neighbors, algorithm="ball_tree", weights="distance"
+                )
                 knn_clf.fit(X, y)
 
-                with open(model_path, 'wb') as f:
+                with open(model_path, "wb") as f:
                     pickle.dump(knn_clf, f)
-                with open("trained_knn_list.clf",'wb') as f:
-                    pickle.dump(y,f)
+                with open("trained_knn_list.clf", "wb") as f:
+                    pickle.dump(y, f)
         except peewee.InterfaceError as e:
             print("PeeWee Interface broken!")
             mysql_db = heartbeat_db.init_db(
                 heartbeat_config.config["db_type"],
                 heartbeat_config.config["db_auth"],
                 heartbeat_config.config["object_storage_type"],
-                heartbeat_config.config["object_storage_auth"]
+                heartbeat_config.config["object_storage_auth"],
             )
             print(e)
         finally:
@@ -242,12 +304,13 @@ def admin_panel():
     else:
         return render_template("admin.html")
 
-@app.route("/upload_new",methods=['POST','GET'])
+
+@app.route("/upload_new", methods=["POST", "GET"])
 def upload_via_frontend():
-    if 'file' not in request.files:
+    if "file" not in request.files:
         return render_template("upload_new.html")
-    file = request.files['file']
-    if file.filename == '':
+    file = request.files["file"]
+    if file.filename == "":
         return render_template("upload_new.html")
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
@@ -261,4 +324,4 @@ def upload_via_frontend():
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0",debug=True)
+    app.run("0.0.0.0", debug=True)
