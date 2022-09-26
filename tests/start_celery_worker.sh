ddd041ebc0daf5763f3cf83703e033e2abe6a49e
mowoe <git@mowoe.com>
2022-09-26 16:20:58 +0200

    Add a new script to push docker image and force message queue purge for celery ci test

--- tests/start_celery_worker.sh
+++ tests/start_celery_worker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd /heartbeat && celery -A distribute_work purge
+cd /heartbeat && celery -A distribute_work purge -f
 cd /heartbeat && celery -A distribute_work worker --loglevel=INFO --uid 1000
